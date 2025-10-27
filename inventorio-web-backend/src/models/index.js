c1e7c5b6cf4185383ff1cb6e45bd9167bc040f12
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2025-10-26 18:50:40 -0700

    Refactor model loading to use ES modules and dynamic import
    Updated the models/index.js file to use ES module syntax, dynamic imports, and JSON import for configuration. This improves compatibility with modern Node.js and streamlines dynamic model loading and association setup.

--- inventorio-web-backend/src/models/index.js
+++ inventorio-web-backend/src/models/index.js
@@ -1,12 +1,18 @@
-'use strict';
-
 import fs from 'fs';
 import path from 'path';
-import Sequelize from 'sequelize';
+import { fileURLToPath } from 'url';
 import process from 'process';
+import Sequelize from 'sequelize';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+
+import configFile from '../../config/config.json' with { type: 'json' };
+const config = configFile[env];
+
 const db = {};
 
 let sequelize;
@@ -16,26 +22,30 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
+// Cargar dinámicamente todos los modelos
+const files = fs
   .readdirSync(__dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.endsWith('.js') &&
+      !file.includes('.test.js')
     );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+for (const file of files) {
+  const { default: modelFunc } = await import(path.join(__dirname, file));
+  const model = modelFunc(sequelize, Sequelize.DataTypes);
+  db[model.name] = model;
+}
+
+// Asociaciones
+for (const modelName of Object.keys(db)) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
-});
+}
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
